52fefff5dd374067024903e8d5ac65e02436ac56
zouwei <huzougege@gmail.com>
2026-02-13 00:03:22 +0800

    fix:vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
-// @ts-expect-error node built-in
 import { readFileSync } from "fs";
 
 const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
